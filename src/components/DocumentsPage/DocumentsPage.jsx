d551294dff2808c9ae0aeef3411026aa0191aa6b
Viacheslav Sazonov <viacheslav.sazonov@seagullinc.co>
2019-12-02 00:51:34 +0300

    Added tabs

--- src/components/DocumentsPage/DocumentsPage.jsx
+++ src/components/DocumentsPage/DocumentsPage.jsx
@@ -7,16 +7,37 @@ import DocumentsReady from './DocumentsReady/DocumentsReady';
 import DocumentsConsumption from './DocumentsConsumption/DocumentsConsumption';
 import DocumentsActs from './DocumentsActs/DocumentsActs';
 
+import { Tab } from 'semantic-ui-react'
+
+const panes = [
+  {
+    menuItem: 'Сведения',
+    render: () => <Tab.Pane attached={false}><DocumentsInfo /></Tab.Pane>,
+  },
+  {
+    menuItem: 'Готовность',
+    render: () => <Tab.Pane attached={false}><DocumentsReady /></Tab.Pane>,
+  },
+  {
+    menuItem: 'Потребление',
+    render: () => <Tab.Pane attached={false}><DocumentsConsumption /></Tab.Pane>,
+  },
+  {
+    menuItem: 'Акты',
+    render: () => <Tab.Pane attached={false}><DocumentsActs /></Tab.Pane>,
+  },
+]
+
 function DocumentsPage() {
 
   return (
     <div className="page-content addresses-page">
       <h1>Документы</h1>
-      <DocumentsNavPage />
-      <DocumentsInfo />
-      <DocumentsReady />
-      <DocumentsConsumption />
-      <DocumentsActs />
+      {/* <DocumentsNavPage /> */}
+      <Tab menu={{ secondary: true, pointing: true }} panes={panes} />
+      
+      
+      
     </div>
   );
 }
